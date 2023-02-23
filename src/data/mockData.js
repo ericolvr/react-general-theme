1640422274ebb3c8c5781ad7b249f9d8422ea1ff
Érico Oliveira <erico@Ericos-MacBook-Pro.local>
2023-02-23 15:53:36 -0300

    Topbar and Themes

---
+++ src/data/mockData.js
@@ -0,0 +1,16 @@
+export const mockDataTeam = [
+    {
+        id: 1,
+        name: "Luciana",
+        email: "luciana@gmail.com",
+        phone: "11-99172-8953",
+        access: "admin"
+    },
+    {
+        id: 1,
+        name: "Erico",
+        email: "erico@gmail.com",
+        phone: "11-98825-9997",
+        access: "user"
+    }
+]
